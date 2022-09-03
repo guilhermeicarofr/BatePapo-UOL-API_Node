3a25d1369156ad912fee44b6ac08b39511233c79
guilhemeicarofr <guilhemeicarofr@gmail.com>
2022-09-03 01:55:51 -0300

    Post new participant req implemented, validating client body data and checking if the user is already a participant. If it is all ok, inserts user in database collection participants and inserts a user entry message in database messages collection. Error treatment is sending status codes only, no messages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
+import joi from 'joi';
 import dotenv from 'dotenv';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc.js';
 
 dotenv.config();
+dayjs.extend(utc);
 
 const server = express();
 server.use(cors());
@@ -11,7 +15,7 @@ server.use(express.json());
 
 const mongo = new MongoClient(process.env.MONGO_URI)
 let db;
-mongo.connect().then(()=> {
+mongo.connect().then(() => {
     db = mongo.db('uol');
     console.log('Connected to Database');
 });
@@ -22,22 +26,44 @@ mongo.connect().then(()=> {
 
 
 
+const userSchema = joi.object({
+    name: joi.string().required()
+});
 
+server.post('/participants', async (req,res) => {
 
+    const time = dayjs().format('HH:mm:ss');
 
+    const { name } = req.body;
+    
+    const validation = userSchema.validate({ name });
 
+    if(validation.error) {
+        console.log(validation.error);
+        return res.sendStatus(422); //melhorar messages
+    }
+    
+    try {
+        const checkuser = await db.collection('participants').findOne({name: name});
+        if(checkuser) {
+            return res.sendStatus(409);
+        }
+        await db.collection('participants').insertOne({name: name, lastStatus: Date.now()});
+        res.sendStatus(201);      
 
+        await db.collection('messages').insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: time
+        });
 
-
-
-
-
-
-
-
-
-
-
+    } catch(error) {
+        console.log(error);
+        res.send(500);
+    }
+});
 
 
 
